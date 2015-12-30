7c610b8b5108e114c9c441a09219170224a96ed4
cpanelsky <10bserver8@gmail.com>
2015-12-30 01:51:23 -0700

    Update domainstats.pl

--- domainstats.pl
+++ domainstats.pl
@@ -1,50 +1,36 @@
 #!/usr/local/cpanel/3rdparty/perl/514/bin/perl
-use strict;
-use warnings;
 use DomainStatus;
 use File::Slurp 'read_file';
+use File::Spec;
+use strict;
+use warnings;
 use IPC::Open3;
-use Term::ANSIColor qw(:constants);
-$Term::ANSIColor::AUTORESET = 1;
-
-#array of domains we're working with
 our $fileName = "/etc/userdatadomains";
 our @links    = read_file( $fileName );
+$Term::ANSIColor::AUTORESET = 1;
+use Term::ANSIColor qw(:constants);
 
-#get the dns/http data 
 sub get_data {
-    #currently mean, but will nice it later(preserves term color)
-    $SIG{'INT'} = sub { print "Caught CTRL+C!\n" };    
-    #tell them what we're doing
-    print "\n\t___Checking HTTP response codes and DNS A records___\n\n";
-    #loop through domains found in @link /etc/userdatadomains file
+  $SIG{'INT'} = sub { print "\nCaught CTRL+C!..."; print RESET " ..Ending...\n" ; exit ; die ; kill HUP => -$$; }; 
+    print "\n\t___Checking HTTP response codes and DNS A records___\n\n\t(be patient..)\n\n";
     foreach my $uDomain ( @links ) {
-        #if we look like the right snip, continue
         if ( $uDomain =~ /(.*):[\s]/ ) {
             our $resource = $1;
-            #I had to wrap open3 in this, because I dont know
             eval {
-                #sets some FH for out and err
                 local ( *HIS_OUT, *HIS_ERR );
-                #calls the routine for the $resource(domain)
                 my $childpid =
                     open3( undef, *HIS_OUT, *HIS_ERR, \&DomainStatus::_get_http_status( "$resource" ), undef );
-                #set the FH data array
                 my @outlines = <HIS_OUT>;          
                 my @errlines = <HIS_ERR>;          
                 print "STDOUT:\n", @outlines, "\n";
                 print "STDERR:\n", @errlines, "\n";
-                #close handles
                 close HIS_OUT;
                 close HIS_ERR;
-                #wait for kids
                 waitpid( $childpid, 0 );
-                #if we have a return code
                 if ( $? ) {
                     print "Child exited with wait status of $?\n";
                 }
             };
-            #yep, second sub in eval here for open3
             eval {
                 local ( *HIS_OUT2, *HIS_ERR2 );
                 my $childpid2 =
@@ -62,19 +48,16 @@ sub get_data {
                 }
             };
         } else {
-            #if we didn't get a domain, lets tell them where we encountered munge
-            print YELLOW "Possible bad Domain data enountered, manually check /etc/userdatadomains file.\n";
+            print YELLOW "Possible bad Domain data enountered, manually check /etc/userdatadomains file after finished.\n";
         }
     }
 }
 
-#run mail sub
-sub shownMailAccounts {
+sub showMailAccounts {
     print "\n\n\t___Mail accounts found:___\n\n";
     &DomainStatus::_get_mail_accounts();
 }
 
-#this sub is to prevent stderr noise
 sub supressERR($) {
     open my $saveout, ">&STDERR";
     open STDERR, '>', File::Spec->devnull();
@@ -83,6 +66,5 @@ sub supressERR($) {
     open STDERR, ">&", $saveout;
 }
 
-#cAll the things
-supressERR( \&get_data );
-shownMailAccounts();
+&supressERR( \&get_data );
+&showMailAccounts();
