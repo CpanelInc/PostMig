9ae20b749e6be84762615f8458b188f69c6ea92e
cpanelsky <10bserver8@gmail.com>
2015-12-29 22:51:20 -0700

    Update DomainStatus.pm

--- DomainStatus.pm
+++ DomainStatus.pm
@@ -12,58 +12,68 @@ our $VERSION = 0.02;
 
 #this is a subroutine to check the http status code for domains
 sub _get_http_status {
+
     #we use lwp/time/ansi for output/commands
     require LWP::UserAgent;
     require Time::HiRes;
     $Term::ANSIColor::AUTORESET = 1;
     use Term::ANSIColor qw(:constants);
+
     #our URL should come in as an argument to the subroutine
-    my $url        = "@_";
+    my $url = "@_";
+
     #we have a basic browser agent and a low timeout for now, here's the request for the URL
-    my $ua         = LWP::UserAgent->new( agent => 'Mozilla/5.0', timeout => '1' );
-    my $req        = HTTP::Request->new( GET => "http://$url" );
-    #here is a time starter for the request to get a difference
-    my $start      = [ Time::HiRes::gettimeofday() ];
-    my $res        = $ua->request( $req );
-    #this *should* be about how long the request took as an http client
-    my $difference = Time::HiRes::tv_interval( $start );
+    my $ua = LWP::UserAgent->new( agent => 'Mozilla/5.0', timeout => '1' );
+    my $req = HTTP::Request->new( GET => "http://$url" );
+    my $res = $ua->request( $req );
+
     #we can parse this for goodies/errors
-    my $body       = $res->decoded_content;
+    my $body = $res->decoded_content;
+
     #this is the status code
-    my $code       = $res->code();
+    my $code = $res->code();
+
     #we can easily check the headers to determine if we had a good request as below
-    my $head       = $res->headers()->as_string;
+    my $head = $res->headers()->as_string;
     print $res->header( "content-type\r\n\r\n" );
+
     #blue seems like a good color for requests that process(for now)
     my $bcode = ( BOLD BLUE $code );
     if ( $head =~ /Client-Peer:[\s](.*):([0-9].*)/ ) {
         my $head2 = "$1:$2";
+
         #here's some terrible formatting, needs improvement
-        printf( "%-35s IP=%-22s Status=%s Time=%-5ss\r\n", $url, $head2, $bcode, $difference );
+        printf( "%-30s IP=%-15s Status=%s  ", $url, $head2, $bcode );
     } else {
+
         #if we didn't see a normal header, let's print the code red with yellow warnings
         my $rcode = ( RED $code );
-        my $error = BOLD YELLOW "ERROR:\t!!!Connect Failed : $url : $rcode!!!";
-        print "$error\n";
+        my $error = BOLD YELLOW "ERROR:\t!!!Connect Failed : $url : $rcode!!! ";
+        print "$error";
     }
 }
 
 #this is a subroutine for DNS checks
 sub _get_dns_data {
-    #I found this here, it worked!
+
+    #We're gonna get libs from here for this
     use lib '/usr/local/cpanel/3rdparty/perl/514/lib64/perl5/cpanel_lib/';
     use IPC::System::Simple qw(system capture $EXITVAL);
+
     #colors again
     $Term::ANSIColor::AUTORESET = 1;
     use Term::ANSIColor qw(:constants);
+
     #here we can get the domain as a parameter and make some dig arguments
     my $domain     = "@_";
     my $cmd        = "dig";
     my @localArgs  = ( "\@localhost", "$domain", "A", "+short", "+tries=1" );
     my @googleArgs = ( "\@8.8.8.8", "$domain", "A", "+short", "+tries=1" );
+
     #so, this uses the lib found to capture stdout of the called system command
     #first we populate it into an array
-    my @googleDNSA    = capture( $cmd, @googleArgs );
+    my @googleDNSA = capture( $cmd, @googleArgs );
+
     #then we reference out the first element because we want a singular return
     #then we do the same for localhost requests
     my $googleDNSR    = \@googleDNSA;
@@ -81,18 +91,21 @@ sub _get_dns_data {
         my $RgoogleIP = ( BOLD RED $googleDNS );
         print "$IPM1" . "$RlocalIP" . "$IPM2" . "$RgoogleIP\n";
     } else {
+
         #if it's defined and matches, we do a normal thing:
         if ( ( $localhostDNS ) && ( "$localhostDNS" eq "$googleDNS" ) ) {
             print "DNS IP: $googleDNS : $domain\n";
         } else {
+
             #else print yellow warning if nothing was returned
-            print YELLOW "WARN: Something bad happened with our DNS request for $domain, DNS possibly not set.\n";
+            print YELLOW "WARN: Something happened to DNS requests for $domain, is DNS set?\n";
         }
     }
 }
 
-
 sub _get_mail_accounts {
+
+    #read in users from passwd
     my @passwd = read_file( "/etc/passwd" );
     my $dir    = '/var/cpanel/users';
     my %user_list;
@@ -101,6 +114,8 @@ sub _get_mail_accounts {
     while ( my $file = readdir( DIR ) ) {
         next if ( $file =~ m/^\./ );
         foreach my $line ( @passwd ) {
+
+            #if we look like a system and cpanel user?
             if ( $line =~ /^$file:[^:]*:[^:]*:[^:]*:[^:]*:([a-z0-9_\/]+):.*/ ) {
                 $user_list{$file} = $1;
             }
@@ -108,12 +123,16 @@ sub _get_mail_accounts {
     }
     closedir( DIR );
 
+    #for the users found, if we aren't root look for an etc dir
     foreach my $user ( keys %user_list ) {
         if ( $user ne "root" ) {
             opendir( ETC, "$user_list{$user}/etc" ) || warn $! . "$user_list{$user}/etc";
             my $path = $user_list{$user};
+
+            #for the domains found in the users etc dir
             while ( my $domain = readdir( ETC ) ) {
-                next if $domain =~ /^\./;    # skip . and .. dirs
+                next if $domain =~ /^\./;   # skip . and .. dirs
+                                            #see if we are a valid etc domain and if so, look for mail users and print
                 if ( -d "$path/etc/$domain/" ) {
                     open( PASSWD, "$path/etc/$domain/passwd" ) || die $! . "/home/$user/etc/$domain/passwd";
                     while ( my $PWLINE = <PASSWD> ) {
@@ -121,8 +140,6 @@ sub _get_mail_accounts {
                         chomp( $user, $domain, $PWLINE );
                         my $PWLINED = "$PWLINE\@$domain";
                         chomp( $PWLINED );
-
-                        #                print "User:$user Domain:$domain Email:" . $PWLINE . "";
                         printf( "User=%-10s Domain=%-35s Email=%s\n", $user, $domain, $PWLINED );
                     }
                     close( PASSWD );
@@ -132,6 +149,5 @@ sub _get_mail_accounts {
         close( ETC );
     }
 }
-
 
 1
