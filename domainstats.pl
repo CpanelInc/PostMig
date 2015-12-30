d5ea3a76286a3e00cf68e86500177151899247bf
Sky <10bserver8@gmail.com>
2015-12-30 14:04:25 -0700

    Unified modulino

--- domainstats.pl
+++ domainstats.pl
@@ -54,31 +54,24 @@ sub _get_http_status {
 
     #our URL should come in as an argument to the subroutine
     my $url = "@_";
-
     #we have a basic browser agent and a low timeout for now, here's the request for the URL
     my $ua = LWP::UserAgent->new( agent => 'Mozilla/5.0', timeout => '1' );
     my $req = HTTP::Request->new( GET => "http://$url" );
     my $res = $ua->request( $req );
-
     #we can parse this for goodies/errors
     my $body = $res->decoded_content;
-
     #this is the status code
     my $code = $res->code();
-
     #we can easily check the headers to determine if we had a good request as below
     my $head = $res->headers()->as_string;
     print $res->header( "content-type\r\n\r\n" );
-
     #blue seems like a good color for requests that process(for now)
     my $bcode = ( BOLD BLUE $code );
     if ( $head =~ /Client-Peer:[\s](.*):([0-9].*)/ ) {
         my $head2 = "$1:$2";
-
         #here's some terrible formatting, needs improvement
         printf("  %-30s PeerIP=%-15s Status=%s\r\n", $url, $head2, $bcode );
     } else {
-
         #if we didn't see a normal header, let's print the code red with yellow warnings
         my $rcode = ( RED $code );
         my $error = BOLD YELLOW "ERROR:\t!!!HTTP Connect Failed : $url : $rcode!!!\n";
@@ -88,27 +81,21 @@ sub _get_http_status {
 
 #this is a subroutine for DNS checks
 sub _get_dns_data {
-
     #I found this here, it worked!
     use lib '/usr/local/cpanel/3rdparty/perl/514/lib64/perl5/cpanel_lib/';
     use IPC::System::Simple qw(system capture $EXITVAL);
-
     #colors again
     $Term::ANSIColor::AUTORESET = 1;
     use Term::ANSIColor qw(:constants);
-
-
 
     #here we can get the domain as a parameter and make some dig arguments
     my $domain     = "@_";
     my $cmd        = "dig";
     my @localArgs  = ( "\@localhost", "$domain", "A", "+short", "+tries=1" );
     my @googleArgs = ( "\@8.8.8.8", "$domain", "A", "+short", "+tries=1" );
-
     #so, this uses the lib found to capture stdout of the called system command
     #first we populate it into an array
     my @googleDNSA = capture( $cmd, @googleArgs );
-
     #then we reference out the first element because we want a singular return
     #then we do the same for localhost requests
     my $googleDNSR    = \@googleDNSA;
@@ -117,7 +104,6 @@ sub _get_dns_data {
     my $localhostDNSR = \@localhostDNSA;
     my $localhostDNS  = $localhostDNSR->[0];
     chomp( $googleDNS, $localhostDNS );
-
     #if the request is defined but doesn't match:
     if ( ( $localhostDNS ) && ( $localhostDNS ne $googleDNS ) ) {
         my $IPM1      = BOLD YELLOW "WARN: Local IP:";
@@ -127,12 +113,10 @@ sub _get_dns_data {
         chomp($RlocalIP,$RgoogleIP);
         print "$IPM1" . "$RlocalIP" . "$IPM2" . "$RgoogleIP\n";
     } else {
-
         #if it's defined and matches, we do a normal thing:
         if ( ( $localhostDNS ) && ( "$localhostDNS" eq "$googleDNS" ) ) {
             print "$domain :: DNS IP: $googleDNS\n";
         } else {
-
             #else print yellow warning if nothing was returned
             print YELLOW "WARN: Something happened to DNS requests for $domain, is DNS set?\n";
         }
@@ -147,7 +131,6 @@ sub _get_mail_accounts {
     my @passwd = read_file( "/etc/passwd" );
     my $dir    = '/var/cpanel/users';
     my %user_list;
-
     opendir( DIR, $dir ) or die $!;
     while ( my $file = readdir( DIR ) ) {
         next if ( $file =~ m/^\./ );
@@ -160,7 +143,6 @@ sub _get_mail_accounts {
         }
     }
     closedir( DIR );
-
     #for the users found, if we aren't root look for an etc dir
     foreach my $user ( keys %user_list ) {
         if ( $user ne "root" ) {
