2791388fd9418efe695be67fe146de278a483f22
cpanelsky <10bserver8@gmail.com>
2015-12-29 22:57:13 -0700

    Update domainstats.pl

--- domainstats.pl
+++ domainstats.pl
@@ -1,67 +1,80 @@
 #!/usr/local/cpanel/3rdparty/perl/514/bin/perl
-# cpanel				           Copyright(c) 2015 cPanel, Inc.
-#                                                           All rights Reserved.
-# copyright@cpanel.net                                         http://cpanel.net
-# This code is subject to the cPanel license. Unauthorized copying is prohibited
-#
-#we dont really get parallel much yet, needs improvement to sync/sort
-use Parallel::ForkManager;
-use DomainStatus;
-use File::Slurp 'read_file';
 use strict;
 use warnings;
+use DomainStatus;
+use File::Slurp 'read_file';
+use IPC::Open3;
+use Term::ANSIColor qw(:constants);
+$Term::ANSIColor::AUTORESET = 1;
 
-# we read in the first "word"(domain) from the userdatadomains file
+#array of domains we're working with
 our $fileName = "/etc/userdatadomains";
 our @links    = read_file( $fileName );
 
+#get the dns/http data 
 sub get_data {
-#we make some parallel fork objects
-    my $pm1 = new Parallel::ForkManager( 1 );
-    my $pm2 = new Parallel::ForkManager( 4 );
-    #loop through the domains, find if defined and begin
+    #currently mean, but will nice it later(preserves term color)
+    $SIG{'INT'} = sub { print "Caught CTRL+C!\n" };    
+    #tell them what we're doing
+    print "\n\t___Checking HTTP response codes and DNS A records___\n\n";
+    #loop through domains found in @link /etc/userdatadomains file
     foreach my $uDomain ( @links ) {
-        #start forking with the pm object
-        $pm1->start and next;
+        #if we look like the right snip, continue
         if ( $uDomain =~ /(.*):[\s]/ ) {
             our $resource = $1;
-        #if we're defined, let's fork off the second pm object
-            $pm2->start and next;
-            #hash of subs to do our work, can add more as we go
-            my %subcalls = ( 'HTTPSTATUS' => \&DomainStatus::_get_http_status( "$resource" ),
-                             'DDNSSTATUS' => \&DomainStatus::_get_dns_data( "$resource" ) );
-            #references to the subs, I'd like to store the return and do something
-            #for now they just exec
-            my $httpref = &{ $subcalls{'HTTPSTATUS'} }();
-            my $dnsref  = &{ $subcalls{'DDNSSTATUS'} }();
-            #clean up after the kids
-            $pm1->finish;
-            $pm1->wait_all_children;
-            $pm2->finish;
-            $pm2->wait_all_children;
-        } else {
-            #if we didnt see a good domain in userdatadomains, we still
-            #have to clean up after the kids, and tell what happened
-            print "Bad Domain data.\n";
-            $pm1->finish;
-            $pm1->wait_all_children;
-            $pm2->finish;
-            $pm2->wait_all_children;
-        }
-        $pm1->finish;
-        $pm1->wait_all_children;
-    }
-    $pm1->finish;
-    $pm1->wait_all_children;
+            #I had to wrap open3 in this, because I dont know
+            eval {
+                #sets some FH for out and err
+                local ( *HIS_OUT, *HIS_ERR );
+                #calls the routine for the $resource(domain)
+                my $childpid =
+                    open3( undef, *HIS_OUT, *HIS_ERR, \&DomainStatus::_get_http_status( "$resource" ), undef );
+                #set the FH data array
+                my @outlines = <HIS_OUT>;          
+                my @errlines = <HIS_ERR>;          
+                print "STDOUT:\n", @outlines, "\n";
+                print "STDERR:\n", @errlines, "\n";
+                #close handles
+                close HIS_OUT;
+                close HIS_ERR;
+                #wait for kids
+                waitpid( $childpid, 0 );
+                #if we have a return code
+                if ( $? ) {
+                    print "Child exited with wait status of $?\n";
+                }
+            };
+            #yep, second sub in eval here for open3
+            eval {
+                local ( *HIS_OUT2, *HIS_ERR2 );
+                my $childpid2 =
+                    open3( undef, *HIS_OUT2, *HIS_ERR2, \&DomainStatus::_get_dns_data( "$resource" ), undef );
+                my @outlines2 = <HIS_OUT2>;
+                my @errlines2 = <HIS_ERR2>;
+                print "STDOUT:\n", @outlines2, "\n";
+                print "STDERR:\n", @errlines2, "\n";
+                close HIS_OUT2;
+                close HIS_ERR2;
+                waitpid( $childpid2, 0 );
 
-    $pm2->finish;
-    $pm2->wait_all_children;
+                if ( $? ) {
+                    print "Child exited with wait status of $?\n";
+                }
+            };
+        } else {
+            #if we didn't get a domain, lets tell them where we encountered munge
+            print YELLOW "Possible bad Domain data enountered, manually check /etc/userdatadomains file.\n";
+        }
+    }
 }
 
+#run mail sub
+sub shownMailAccounts {
+    print "\n\n\t___Mail accounts found:___\n\n";
+    &DomainStatus::_get_mail_accounts();
+}
 
-#this is a wrapper sub to disable stderr because I can't properly
-#prevent the "Not a CODE reference error when calling my subroutines
-#with a dynamic parameter((the domain)I *think* because the params anyway)
+#this sub is to prevent stderr noise
 sub supressERR($) {
     open my $saveout, ">&STDERR";
     open STDERR, '>', File::Spec->devnull();
@@ -70,5 +83,6 @@ sub supressERR($) {
     open STDERR, ">&", $saveout;
 }
 
+#cAll the things
 supressERR( \&get_data );
-&DomainStatus::_get_mail_accounts();
+shownMailAccounts();
